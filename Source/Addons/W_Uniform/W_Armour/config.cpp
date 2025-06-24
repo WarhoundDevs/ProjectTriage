f2751838134e1883678d0fb2b62dadf8352bc8dc
Thomas Bainbridge <leohaliwell@gmail.com>
2025-06-24 17:28:19 -0500

    Can we get Base working?

--- Source/Addons/W_Uniform/W_Armour/config.cpp
+++ Source/Addons/W_Uniform/W_Armour/config.cpp
@@ -99,11 +99,11 @@ class CfgVehicles {
             "MA_Armor\data\Uniforms\Marine\data\Color_Variants\MA_Woodland_TrooperCollar_CO.paa",
             "MA_Armor\data\Uniforms\Marine\data\Color_Variants\TrooperSoftpadding_CO.paa",
             "MA_Armor\data\Uniforms\Marine\data\Color_Variants\TrooperArmorStraps_CO.paa",
-            "MA_Armor\data\Uniforms\Marine\data\Color_Variants\MA_green_TrooperUpperArmor_CO.paa", // Will need new texture
-            "MA_Armor\data\Uniforms\Marine\data\Color_Variants\MA_green_TrooperLowerArmor_CO.paa",
-            "W_Uniform\W_Armour\Infantry\Base\MA_green_TrooperShoulders_CO.paa",
-            "W_Uniform\W_Armour\Infantry\Base\MA_green_TrooperShoulders_CO.paa",
-            "W_Uniform\W_Armour\Infantry\Base\MA_green_TrooperShoulders_CO.paa",
+            "MA_Armor\data\Uniforms\Marine\data\Color_Variants\MA_brown_TrooperUpperArmor_CO.paa", // Will need new texture
+            "MA_Armor\data\Uniforms\Marine\data\Color_Variants\MA_brown_TrooperLowerArmor_CO.paa",
+            "\W_Uniform\W_Armour\Infantry\Base\22nd_Shoulders_Base.paa",
+            "\W_Uniform\W_Armour\Infantry\Base\22nd_Shoulders_Base.paa",
+            "\W_Uniform\W_Armour\Infantry\Base\22nd_Shoulders_Base.paa",
         };
     }; // Close 22nd_ArmourBaseVar1_Light
     class 22nd_ArmourBaseVar1_Medium: MA_Marine_BDU_Base {
@@ -139,11 +139,11 @@ class CfgVehicles {
             "MA_Armor\data\Uniforms\Marine\data\Color_Variants\MA_Woodland_TrooperCollar_CO.paa",
             "MA_Armor\data\Uniforms\Marine\data\Color_Variants\TrooperSoftpadding_CO.paa",
             "MA_Armor\data\Uniforms\Marine\data\Color_Variants\TrooperArmorStraps_CO.paa",
-            "MA_Armor\data\Uniforms\Marine\data\Color_Variants\MA_green_TrooperUpperArmor_CO.paa", // Will need new texture
-            "MA_Armor\data\Uniforms\Marine\data\Color_Variants\MA_green_TrooperLowerArmor_CO.paa",
-            "W_Uniform\W_Armour\Infantry\Base\MA_green_TrooperShoulders_CO.paa",
-            "W_Uniform\W_Armour\Infantry\Base\MA_green_TrooperShoulders_CO.paa",
-            "W_Uniform\W_Armour\Infantry\Base\MA_green_TrooperShoulders_CO.paa",
+            "MA_Armor\data\Uniforms\Marine\data\Color_Variants\MA_brown_TrooperUpperArmor_CO.paa", // Will need new texture
+            "MA_Armor\data\Uniforms\Marine\data\Color_Variants\MA_brown_TrooperLowerArmor_CO.paa",
+            "\W_Uniform\W_Armour\Infantry\Base\22nd_Shoulders_Base.paa",
+            "\W_Uniform\W_Armour\Infantry\Base\22nd_Shoulders_Base.paa",
+            "\W_Uniform\W_Armour\Infantry\Base\22nd_Shoulders_Base.paa",
         };
     }; // Close 22nd_ArmourBaseVar1_Medium
     class 22nd_ArmourBaseVar1_Heavy: MA_Marine_BDU_Base {
@@ -179,11 +179,11 @@ class CfgVehicles {
             "MA_Armor\data\Uniforms\Marine\data\Color_Variants\MA_Woodland_TrooperCollar_CO.paa",
             "MA_Armor\data\Uniforms\Marine\data\Color_Variants\TrooperSoftpadding_CO.paa",
             "MA_Armor\data\Uniforms\Marine\data\Color_Variants\TrooperArmorStraps_CO.paa",
-            "MA_Armor\data\Uniforms\Marine\data\Color_Variants\MA_green_TrooperUpperArmor_CO.paa", // Will need new texture
-            "MA_Armor\data\Uniforms\Marine\data\Color_Variants\MA_green_TrooperLowerArmor_CO.paa",
-            "W_Uniform\W_Armour\Infantry\Base\MA_green_TrooperShoulders_CO.paa",
-            "W_Uniform\W_Armour\Infantry\Base\MA_green_TrooperShoulders_CO.paa",
-            "W_Uniform\W_Armour\Infantry\Base\MA_green_TrooperShoulders_CO.paa",
+            "MA_Armor\data\Uniforms\Marine\data\Color_Variants\MA_brown_TrooperUpperArmor_CO.paa", // Will need new texture
+            "MA_Armor\data\Uniforms\Marine\data\Color_Variants\MA_brown_TrooperLowerArmor_CO.paa",
+            "\W_Uniform\W_Armour\Infantry\Base\22nd_Shoulders_Base.paa",
+            "\W_Uniform\W_Armour\Infantry\Base\22nd_Shoulders_Base.paa",
+            "\W_Uniform\W_Armour\Infantry\Base\22nd_Shoulders_Base.paa",
         };
     }; // Close 22nd_ArmourBaseVar1_Heavy
 };
@@ -231,11 +231,11 @@ class CfgWeapons {
                 "MA_Armor\data\Uniforms\Marine\data\Color_Variants\MA_Woodland_TrooperCollar_CO.paa", // Collar texture
                 "MA_Armor\data\Uniforms\Marine\data\Color_Variants\TrooperSoftpadding_CO.paa", // Soft padding texture
                 "MA_Armor\data\Uniforms\Marine\data\Color_Variants\TrooperArmorStraps_CO.paa", // Armor straps texture
-                "MA_Armor\data\Uniforms\Marine\data\Color_Variants\MA_green_TrooperUpperArmor_CO.paa", // Upper armor texture
-                "MA_Armor\data\Uniforms\Marine\data\Color_Variants\MA_green_TrooperLowerArmor_CO.paa", // Lower armor texture
-                "\W_Uniform\W_Armour\Infantry\Base\MA_green_TrooperShoulders_CO.paa", // Shoulders texture
-                "\W_Uniform\W_Armour\Infantry\Base\MA_green_TrooperShoulders_CO.paa", // Shoulders texture (duplicate)
-                "\W_Uniform\W_Armour\Infantry\Base\MA_green_TrooperShoulders_CO.paa"  // Shoulders texture (duplicate)
+                "MA_Armor\data\Uniforms\Marine\data\Color_Variants\MA_brown_TrooperUpperArmor_CO.paa", // Upper armor texture
+                "MA_Armor\data\Uniforms\Marine\data\Color_Variants\MA_brown_TrooperLowerArmor_CO.paa", // Lower armor texture
+                "\W_Uniform\W_Armour\Infantry\Base\22nd_Shoulders_Base.paa", // Shoulders texture
+                "\W_Uniform\W_Armour\Infantry\Base\22nd_Shoulders_Base.paa", // Shoulders texture (duplicate)
+                "\W_Uniform\W_Armour\Infantry\Base\22nd_Shoulders_Base.paa"  // Shoulders texture (duplicate)
             };
         };
     }; // Close 22nd_ArmourBaseVar1_Light
@@ -279,12 +279,11 @@ class CfgWeapons {
                 "MA_Armor\data\Uniforms\Marine\data\Color_Variants\MA_Woodland_TrooperCollar_CO.paa", // Collar texture
                 "MA_Armor\data\Uniforms\Marine\data\Color_Variants\TrooperSoftpadding_CO.paa", // Soft padding texture
                 "MA_Armor\data\Uniforms\Marine\data\Color_Variants\TrooperArmorStraps_CO.paa", // Armor straps texture
-                "MA_Armor\data\Uniforms\Marine\data\Color_Variants\MA_green_TrooperUpperArmor_CO.paa", // Upper armor texture
-                "MA_Armor\data\Uniforms\Marine\data\Color_Variants\MA_green_TrooperLowerArmor_CO.paa", // Lower armor texture
-                "\W_Uniform\W_Armour\Infantry\Base\MA_green_TrooperShoulders_CO.paa", // Shoulders texture
-                "\W_Uniform\W_Armour\Infantry\Base\MA_green_TrooperShoulders_CO.paa", // Shoulders texture (duplicate)
-                "\W_Uniform\W_Armour\Infantry\Base\MA_green_TrooperShoulders_CO.paa"  // Shoulders texture (duplicate)
-            };
+                "MA_Armor\data\Uniforms\Marine\data\Color_Variants\MA_brown_TrooperUpperArmor_CO.paa", // Upper armor texture
+                "MA_Armor\data\Uniforms\Marine\data\Color_Variants\MA_brown_TrooperLowerArmor_CO.paa", // Lower armor texture
+                "\W_Uniform\W_Armour\Infantry\Base\22nd_Shoulders_Base.paa", // Shoulders texture
+                "\W_Uniform\W_Armour\Infantry\Base\22nd_Shoulders_Base.paa", // Shoulders texture (duplicate)
+                "\W_Uniform\W_Armour\Infantry\Base\22nd_Shoulders_Base.paa"  // Shoulders texture (duplicate)
         };
     }; // Close 22nd_ArmourBaseVar1_Medium
     class 22nd_ArmourBaseVar1_Heavy: MA_Marine_BDU_Base {
@@ -327,12 +326,11 @@ class CfgWeapons {
                 "MA_Armor\data\Uniforms\Marine\data\Color_Variants\MA_Woodland_TrooperCollar_CO.paa", // Collar texture
                 "MA_Armor\data\Uniforms\Marine\data\Color_Variants\TrooperSoftpadding_CO.paa", // Soft padding texture
                 "MA_Armor\data\Uniforms\Marine\data\Color_Variants\TrooperArmorStraps_CO.paa", // Armor straps texture
-                "MA_Armor\data\Uniforms\Marine\data\Color_Variants\MA_green_TrooperUpperArmor_CO.paa", // Upper armor texture
-                "MA_Armor\data\Uniforms\Marine\data\Color_Variants\MA_green_TrooperLowerArmor_CO.paa", // Lower armor texture
-                "\W_Uniform\W_Armour\Infantry\Base\MA_green_TrooperShoulders_CO.paa", // Shoulders texture
-                "\W_Uniform\W_Armour\Infantry\Base\MA_green_TrooperShoulders_CO.paa", // Shoulders texture (duplicate)
-                "\W_Uniform\W_Armour\Infantry\Base\MA_green_TrooperShoulders_CO.paa"  // Shoulders texture (duplicate)
-            };
+                "MA_Armor\data\Uniforms\Marine\data\Color_Variants\MA_brown_TrooperUpperArmor_CO.paa", // Upper armor texture
+                "MA_Armor\data\Uniforms\Marine\data\Color_Variants\MA_brown_TrooperLowerArmor_CO.paa", // Lower armor texture
+                "\W_Uniform\W_Armour\Infantry\Base\22nd_Shoulders_Base.paa", // Shoulders texture
+                "\W_Uniform\W_Armour\Infantry\Base\22nd_Shoulders_Base.paa", // Shoulders texture (duplicate)
+                "\W_Uniform\W_Armour\Infantry\Base\22nd_Shoulders_Base.paa"  // Shoulders texture (duplicate)
         };
     }; // Close 22nd_ArmourBaseVar1_Heavy
 };
