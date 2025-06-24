343216ede8c5a0eda9de5b97b5beab0425cf61ea
Thomas Bainbridge <leohaliwell@gmail.com>
2025-06-24 16:50:01 -0500

    Complete config rewrite. (R)

--- Source/mod.cpp
+++ Source/mod.cpp
@@ -1,8 +1,8 @@
-name = "Warhound Company Gear & Equipment";
+name = "22nd RDF Arms Room";
 picture = "22nd_rdf.paa"; //Image that shows in the ARMA 3 Launcher.
 overview = "Warhound Company Aux Mod";
 logo = "22nd_rdf.paa"; // Logo that shows next to the mod in the launcher.
 logoOver = "22nd_rdf.paa"; // Logo when hovered over in the launcher
-tooltip = "Warhound Company Aux Mod";
-tooltipOwned = "Warhound Company Gear & Equipment installed";
+tooltip = "22nd RDF Arms Room";
+tooltipOwned = "22nd RDF Arms Room installed";
 author = "Warhound Company Mod Team"; // Your name or team name
