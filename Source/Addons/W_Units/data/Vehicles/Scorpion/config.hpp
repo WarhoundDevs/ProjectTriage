8b8a0b7a71bf54212533c0ab492623c6d3b379dd
byorh2 <59620914+byorh2@users.noreply.github.com>
2025-07-13 13:14:01 -0400

    Driverless Lancer Scorpion plus fixed textures
    Added Lancer Driverless scorpion with same textures as regular Lancer scorpion. Also fixed Scorpion textures for Kamea.

--- Source/Addons/W_Units/data/Vehicles/Scorpion/config.hpp
+++ Source/Addons/W_Units/data/Vehicles/Scorpion/config.hpp
@@ -1,4 +1,5 @@
 class OPTRE_M808BM_UNSC;
+class OPTRE_M808BM_UNSC_Driverless;
 class DMNS_M808B_F;
 class 22nd_M808BM_Scorpion: OPTRE_M808BM_UNSC
 {
@@ -12,14 +13,14 @@ class 22nd_M808BM_Scorpion: OPTRE_M808BM_UNSC
 	side=1;
 	hiddenSelectionsTextures[] =
 	{
-		"Warhound_Units\data\Vehicles\Scorpion\Urban\22nd_Scorp_Base_Black_co.paa",
-		"Warhound_Units\data\Vehicles\Scorpion\Urban\22nd_Scorp_Tur_Black_co.paa",
-		"Warhound_Units\data\Vehicles\Scorpion\22nd_det_3_black_co.paa",
-		"Warhound_Units\data\Vehicles\Scorpion\22nd_scorp_decal_blank_ca.paa",
-		"Warhound_Units\data\Vehicles\Scorpion\Urban\Scorp_net_wood_ca.paa",
-		"Warhound_Units\data\Vehicles\Scorpion\Urban\Scorp_net_wood_ca.paa",
-		"Warhound_Units\data\Vehicles\Scorpion\Urban\22nd_Scorp_Mine_Roller_Black_co.paa",
-		"Warhound_Units\data\Vehicles\Scorpion\Urban\22nd_Scorp_MG_Black_co.paa"
+		"W_Units\data\Vehicles\Scorpion\Urban\22nd_Scorp_Base_Black_co.paa",
+		"W_Units\data\Vehicles\Scorpion\Urban\22nd_Scorp_Tur_Black_co.paa",
+		"W_Units\data\Vehicles\Scorpion\22nd_det_3_black_co.paa",
+		"W_Units\data\Vehicles\Scorpion\22nd_scorp_decal_blank_ca.paa",
+		"W_Units\data\Vehicles\Scorpion\Urban\Scorp_net_wood_ca.paa",
+		"W_Units\data\Vehicles\Scorpion\Urban\Scorp_net_wood_ca.paa",
+		"W_Units\data\Vehicles\Scorpion\Urban\22nd_Scorp_Mine_Roller_Black_co.paa",
+		"W_Units\data\Vehicles\Scorpion\Urban\22nd_Scorp_MG_Black_co.paa"
 	};
 	textureList[] =
 	{
@@ -40,14 +41,14 @@ class 22nd_M808BM_Scorpion: OPTRE_M808BM_UNSC
 			author = "Kidd";
 			textures[] =
 			{
-				"Warhound_Units\data\Vehicles\Scorpion\Urban\22nd_Scorp_Base_Black_co.paa",
-				"Warhound_Units\data\Vehicles\Scorpion\Urban\22nd_Scorp_Tur_Black_co.paa",
-				"Warhound_Units\data\Vehicles\Scorpion\22nd_det_3_black_co.paa",
-				"Warhound_Units\data\Vehicles\Scorpion\22nd_scorp_decal_blank_ca.paa",
-				"Warhound_Units\data\Vehicles\Scorpion\Urban\Scorp_net_wood_ca.paa",
-				"Warhound_Units\data\Vehicles\Scorpion\Urban\Scorp_net_wood_ca.paa",
-				"Warhound_Units\data\Vehicles\Scorpion\Urban\22nd_Scorp_Mine_Roller_Black_co.paa",
-				"Warhound_Units\data\Vehicles\Scorpion\Urban\22nd_Scorp_MG_Black_co.paa"
+				"W_Units\data\Vehicles\Scorpion\Urban\22nd_Scorp_Base_Black_co.paa",
+				"W_Units\data\Vehicles\Scorpion\Urban\22nd_Scorp_Tur_Black_co.paa",
+				"W_Units\data\Vehicles\Scorpion\22nd_det_3_black_co.paa",
+				"W_Units\data\Vehicles\Scorpion\22nd_scorp_decal_blank_ca.paa",
+				"W_Units\data\Vehicles\Scorpion\Urban\Scorp_net_wood_ca.paa",
+				"W_Units\data\Vehicles\Scorpion\Urban\Scorp_net_wood_ca.paa",
+				"W_Units\data\Vehicles\Scorpion\Urban\22nd_Scorp_Mine_Roller_Black_co.paa",
+				"W_Units\data\Vehicles\Scorpion\Urban\22nd_Scorp_MG_Black_co.paa"
 			};
 		};
 		class Scorp_Arid
@@ -56,14 +57,14 @@ class 22nd_M808BM_Scorpion: OPTRE_M808BM_UNSC
 			author = "Kidd";
 			textures[] =
 			{
-				"Warhound_Units\data\Vehicles\Scorpion\Arid\22nd_Scorp_Base_Desert_co.paa",
-				"Warhound_Units\data\Vehicles\Scorpion\Arid\22nd_Scorp_Tur_Desert_co.paa",
-				"Warhound_Units\data\Vehicles\Scorpion\22nd_det_3_black_co.paa",
-				"Warhound_Units\data\Vehicles\Scorpion\22nd_scorp_decal_blank_ca.paa",
-				"Warhound_Units\data\Vehicles\Scorpion\Arid\Scorp_net_desert_ca.paa",
-				"Warhound_Units\data\Vehicles\Scorpion\Arid\Scorp_net_desert_ca.paa",
-				"Warhound_Units\data\Vehicles\Scorpion\Arid\22nd_Scorp_Mine_Roller_Desert_co.paa",
-				"Warhound_Units\data\Vehicles\Scorpion\Arid\22nd_Scorp_MG_Desert_co.paa"
+				"W_Units\data\Vehicles\Scorpion\Arid\22nd_Scorp_Base_Desert_co.paa",
+				"W_Units\data\Vehicles\Scorpion\Arid\22nd_Scorp_Tur_Desert_co.paa",
+				"W_Units\data\Vehicles\Scorpion\22nd_det_3_black_co.paa",
+				"W_Units\data\Vehicles\Scorpion\22nd_scorp_decal_blank_ca.paa",
+				"W_Units\data\Vehicles\Scorpion\Arid\Scorp_net_desert_ca.paa",
+				"W_Units\data\Vehicles\Scorpion\Arid\Scorp_net_desert_ca.paa",
+				"W_Units\data\Vehicles\Scorpion\Arid\22nd_Scorp_Mine_Roller_Desert_co.paa",
+				"W_Units\data\Vehicles\Scorpion\Arid\22nd_Scorp_MG_Desert_co.paa"
 			};
 		};
 		class Scorp_Woodland
@@ -72,14 +73,14 @@ class 22nd_M808BM_Scorpion: OPTRE_M808BM_UNSC
 			author = "Kidd";
 			textures[] =
 			{
-				"Warhound_Units\data\Vehicles\Scorpion\Woodland\22nd_Scorp_Base_Woodland_co.paa",
-				"Warhound_Units\data\Vehicles\Scorpion\Woodland\22nd_Scorp_Tur_Woodland_co.paa",
-				"Warhound_Units\data\Vehicles\Scorpion\22nd_det_3_black_co.paa",
-				"Warhound_Units\data\Vehicles\Scorpion\22nd_scorp_decal_blank_ca.paa",
-				"Warhound_Units\data\Vehicles\Scorpion\Woodland\Scorp_net_jungle_ca.paa",
-				"Warhound_Units\data\Vehicles\Scorpion\Woodland\Scorp_net_jungle_ca.paa",
-				"Warhound_Units\data\Vehicles\Scorpion\Woodland\22nd_Scorp_Mine_Roller_Woodland_co.paa",
-				"Warhound_Units\data\Vehicles\Scorpion\Woodland\22nd_Scorp_MG_Woodland_co.paa"
+				"W_Units\data\Vehicles\Scorpion\Woodland\22nd_Scorp_Base_Woodland_co.paa",
+				"W_Units\data\Vehicles\Scorpion\Woodland\22nd_Scorp_Tur_Woodland_co.paa",
+				"W_Units\data\Vehicles\Scorpion\22nd_det_3_black_co.paa",
+				"W_Units\data\Vehicles\Scorpion\22nd_scorp_decal_blank_ca.paa",
+				"W_Units\data\Vehicles\Scorpion\Woodland\Scorp_net_jungle_ca.paa",
+				"W_Units\data\Vehicles\Scorpion\Woodland\Scorp_net_jungle_ca.paa",
+				"W_Units\data\Vehicles\Scorpion\Woodland\22nd_Scorp_Mine_Roller_Woodland_co.paa",
+				"W_Units\data\Vehicles\Scorpion\Woodland\22nd_Scorp_MG_Woodland_co.paa"
 			};
 		};
 		class Scorp_Arctic
@@ -88,14 +89,14 @@ class 22nd_M808BM_Scorpion: OPTRE_M808BM_UNSC
 			author = "Kidd";
 			textures[] =
 			{
-				"Warhound_Units\data\Vehicles\Scorpion\Arctic\22nd_Scorp_Base_Arctic_co.paa",
-				"Warhound_Units\data\Vehicles\Scorpion\Arctic\22nd_Scorp_Tur_Arctic_co.paa",
-				"Warhound_Units\data\Vehicles\Scorpion\22nd_det_3_black_co.paa",
-				"Warhound_Units\data\Vehicles\Scorpion\22nd_scorp_decal_blank_ca.paa",
-				"Warhound_Units\data\Vehicles\Scorpion\Arctic\Scorp_net_tundra_ca.paa",
-				"Warhound_Units\data\Vehicles\Scorpion\Arctic\Scorp_net_tundra_ca.paa",
-				"Warhound_Units\data\Vehicles\Scorpion\Arctic\22nd_Scorp_Mine_Roller_Arctic_co.paa",
-				"Warhound_Units\data\Vehicles\Scorpion\Arctic\22nd_Scorp_MG_Arctic_co.paa"
+				"W_Units\data\Vehicles\Scorpion\Arctic\22nd_Scorp_Base_Arctic_co.paa",
+				"W_Units\data\Vehicles\Scorpion\Arctic\22nd_Scorp_Tur_Arctic_co.paa",
+				"W_Units\data\Vehicles\Scorpion\22nd_det_3_black_co.paa",
+				"W_Units\data\Vehicles\Scorpion\22nd_scorp_decal_blank_ca.paa",
+				"W_Units\data\Vehicles\Scorpion\Arctic\Scorp_net_tundra_ca.paa",
+				"W_Units\data\Vehicles\Scorpion\Arctic\Scorp_net_tundra_ca.paa",
+				"W_Units\data\Vehicles\Scorpion\Arctic\22nd_Scorp_Mine_Roller_Arctic_co.paa",
+				"W_Units\data\Vehicles\Scorpion\Arctic\22nd_Scorp_MG_Arctic_co.paa"
 			};
 		};
 	};
@@ -112,14 +113,14 @@ class 22nd_M808BM_Lancer_Scorpion: OPTRE_M808BM_UNSC
 	side=1;
 	hiddenSelectionsTextures[] =
 	{
-		"Warhound_Units\data\Vehicles\Scorpion\Urban\22nd_Scorp_Base_Black_co.paa",
-		"Warhound_Units\data\Vehicles\Scorpion\Lancer\22nd_Scorp_Tur_Lancer_Black_co.paa",
-		"Warhound_Units\data\Vehicles\Scorpion\22nd_det_3_black_co.paa",
-		"Warhound_Units\data\Vehicles\Scorpion\Lancer\22nd_scorp_decal_Lancer_co.paa",
-		"Warhound_Units\data\Vehicles\Scorpion\Urban\Scorp_net_wood_ca.paa",
-		"Warhound_Units\data\Vehicles\Scorpion\Urban\Scorp_net_wood_ca.paa",
-		"Warhound_Units\data\Vehicles\Scorpion\Urban\22nd_Scorp_Mine_Roller_Black_co.paa",
-		"Warhound_Units\data\Vehicles\Scorpion\Urban\22nd_Scorp_MG_Black_co.paa"
+		"W_Units\data\Vehicles\Scorpion\Urban\22nd_Scorp_Base_Black_co.paa",
+		"W_Units\data\Vehicles\Scorpion\Lancer\22nd_Scorp_Tur_Lancer_Black_co.paa",
+		"W_Units\data\Vehicles\Scorpion\22nd_det_3_black_co.paa",
+		"W_Units\data\Vehicles\Scorpion\Lancer\22nd_scorp_decal_Lancer_co.paa",
+		"W_Units\data\Vehicles\Scorpion\Urban\Scorp_net_wood_ca.paa",
+		"W_Units\data\Vehicles\Scorpion\Urban\Scorp_net_wood_ca.paa",
+		"W_Units\data\Vehicles\Scorpion\Urban\22nd_Scorp_Mine_Roller_Black_co.paa",
+		"W_Units\data\Vehicles\Scorpion\Urban\22nd_Scorp_MG_Black_co.paa"
 	};
 	textureList[] =
 	{
@@ -140,14 +141,14 @@ class 22nd_M808BM_Lancer_Scorpion: OPTRE_M808BM_UNSC
 			author = "Kidd";
 			textures[] =
 			{
-				"Warhound_Units\data\Vehicles\Scorpion\Urban\22nd_Scorp_Base_Black_co.paa",
-				"Warhound_Units\data\Vehicles\Scorpion\Lancer\22nd_Scorp_Tur_Lancer_Black_co.paa",
-				"Warhound_Units\data\Vehicles\Scorpion\22nd_det_3_black_co.paa",
-				"Warhound_Units\data\Vehicles\Scorpion\Lancer\22nd_scorp_decal_Lancer_co.paa",
-				"Warhound_Units\data\Vehicles\Scorpion\Urban\Scorp_net_wood_ca.paa",
-				"Warhound_Units\data\Vehicles\Scorpion\Urban\Scorp_net_wood_ca.paa",
-				"Warhound_Units\data\Vehicles\Scorpion\Urban\22nd_Scorp_Mine_Roller_Black_co.paa",
-				"Warhound_Units\data\Vehicles\Scorpion\Urban\22nd_Scorp_MG_Black_co.paa"
+				"W_Units\data\Vehicles\Scorpion\Urban\22nd_Scorp_Base_Black_co.paa",
+				"W_Units\data\Vehicles\Scorpion\Lancer\22nd_Scorp_Tur_Lancer_Black_co.paa",
+				"W_Units\data\Vehicles\Scorpion\22nd_det_3_black_co.paa",
+				"W_Units\data\Vehicles\Scorpion\Lancer\22nd_scorp_decal_Lancer_co.paa",
+				"W_Units\data\Vehicles\Scorpion\Urban\Scorp_net_wood_ca.paa",
+				"W_Units\data\Vehicles\Scorpion\Urban\Scorp_net_wood_ca.paa",
+				"W_Units\data\Vehicles\Scorpion\Urban\22nd_Scorp_Mine_Roller_Black_co.paa",
+				"W_Units\data\Vehicles\Scorpion\Urban\22nd_Scorp_MG_Black_co.paa"
 			};
 		};
 		class Lancer_Scorp_Arid
@@ -156,14 +157,14 @@ class 22nd_M808BM_Lancer_Scorpion: OPTRE_M808BM_UNSC
 			author = "Kidd";
 			textures[] =
 			{
-				"Warhound_Units\data\Vehicles\Scorpion\Arid\22nd_Scorp_Base_Desert_co.paa",
-				"Warhound_Units\data\Vehicles\Scorpion\Lancer\22nd_Scorp_Tur_Lancer_Desert_co.paa",
-				"Warhound_Units\data\Vehicles\Scorpion\22nd_det_3_black_co.paa",
-				"Warhound_Units\data\Vehicles\Scorpion\Lancer\22nd_scorp_decal_Lancer_co.paa",
-				"Warhound_Units\data\Vehicles\Scorpion\Arid\Scorp_net_desert_ca.paa",
-				"Warhound_Units\data\Vehicles\Scorpion\Arid\Scorp_net_desert_ca.paa",
-				"Warhound_Units\data\Vehicles\Scorpion\Arid\22nd_Scorp_Mine_Roller_Desert_co.paa",
-				"Warhound_Units\data\Vehicles\Scorpion\Arid\22nd_Scorp_MG_Desert_co.paa"
+				"W_Units\data\Vehicles\Scorpion\Arid\22nd_Scorp_Base_Desert_co.paa",
+				"W_Units\data\Vehicles\Scorpion\Lancer\22nd_Scorp_Tur_Lancer_Desert_co.paa",
+				"W_Units\data\Vehicles\Scorpion\22nd_det_3_black_co.paa",
+				"W_Units\data\Vehicles\Scorpion\Lancer\22nd_scorp_decal_Lancer_co.paa",
+				"W_Units\data\Vehicles\Scorpion\Arid\Scorp_net_desert_ca.paa",
+				"W_Units\data\Vehicles\Scorpion\Arid\Scorp_net_desert_ca.paa",
+				"W_Units\data\Vehicles\Scorpion\Arid\22nd_Scorp_Mine_Roller_Desert_co.paa",
+				"W_Units\data\Vehicles\Scorpion\Arid\22nd_Scorp_MG_Desert_co.paa"
 			};
 		};
 		class Lancer_Scorp_Woodland
@@ -172,14 +173,14 @@ class 22nd_M808BM_Lancer_Scorpion: OPTRE_M808BM_UNSC
 			author = "Kidd";
 			textures[] =
 			{
-				"Warhound_Units\data\Vehicles\Scorpion\Woodland\22nd_Scorp_Base_Woodland_co.paa",
-				"Warhound_Units\data\Vehicles\Scorpion\Lancer\22nd_Scorp_Tur_Lancer_Woodland_co.paa",
-				"Warhound_Units\data\Vehicles\Scorpion\22nd_det_3_black_co.paa",
-				"Warhound_Units\data\Vehicles\Scorpion\Lancer\22nd_scorp_decal_Lancer_co.paa",
-				"Warhound_Units\data\Vehicles\Scorpion\Woodland\Scorp_net_jungle_ca.paa",
-				"Warhound_Units\data\Vehicles\Scorpion\Woodland\Scorp_net_jungle_ca.paa",
-				"Warhound_Units\data\Vehicles\Scorpion\Woodland\22nd_Scorp_Mine_Roller_Woodland_co.paa",
-				"Warhound_Units\data\Vehicles\Scorpion\Woodland\22nd_Scorp_MG_Woodland_co.paa"
+				"W_Units\data\Vehicles\Scorpion\Woodland\22nd_Scorp_Base_Woodland_co.paa",
+				"W_Units\data\Vehicles\Scorpion\Lancer\22nd_Scorp_Tur_Lancer_Woodland_co.paa",
+				"W_Units\data\Vehicles\Scorpion\22nd_det_3_black_co.paa",
+				"W_Units\data\Vehicles\Scorpion\Lancer\22nd_scorp_decal_Lancer_co.paa",
+				"W_Units\data\Vehicles\Scorpion\Woodland\Scorp_net_jungle_ca.paa",
+				"W_Units\data\Vehicles\Scorpion\Woodland\Scorp_net_jungle_ca.paa",
+				"W_Units\data\Vehicles\Scorpion\Woodland\22nd_Scorp_Mine_Roller_Woodland_co.paa",
+				"W_Units\data\Vehicles\Scorpion\Woodland\22nd_Scorp_MG_Woodland_co.paa"
 			};
 		};
 		class Lancer_Scorp_Arctic
@@ -188,14 +189,114 @@ class 22nd_M808BM_Lancer_Scorpion: OPTRE_M808BM_UNSC
 			author = "Kidd";
 			textures[] =
 			{
-				"Warhound_Units\data\Vehicles\Scorpion\Arctic\22nd_Scorp_Base_Arctic_co.paa",
-				"Warhound_Units\data\Vehicles\Scorpion\Lancer\22nd_Scorp_Tur_Lancer_Arctic_co.paa",
-				"Warhound_Units\data\Vehicles\Scorpion\22nd_det_3_black_co.paa",
-				"Warhound_Units\data\Vehicles\Scorpion\Lancer\22nd_scorp_decal_Lancer_co.paa",
-				"Warhound_Units\data\Vehicles\Scorpion\Arctic\Scorp_net_tundra_ca.paa",
-				"Warhound_Units\data\Vehicles\Scorpion\Arctic\Scorp_net_tundra_ca.paa",
-				"Warhound_Units\data\Vehicles\Scorpion\Arctic\22nd_Scorp_Mine_Roller_Arctic_co.paa",
-				"Warhound_Units\data\Vehicles\Scorpion\Arctic\22nd_Scorp_MG_Arctic_co.paa"
+				"W_Units\data\Vehicles\Scorpion\Arctic\22nd_Scorp_Base_Arctic_co.paa",
+				"W_Units\data\Vehicles\Scorpion\Lancer\22nd_Scorp_Tur_Lancer_Arctic_co.paa",
+				"W_Units\data\Vehicles\Scorpion\22nd_det_3_black_co.paa",
+				"W_Units\data\Vehicles\Scorpion\Lancer\22nd_scorp_decal_Lancer_co.paa",
+				"W_Units\data\Vehicles\Scorpion\Arctic\Scorp_net_tundra_ca.paa",
+				"W_Units\data\Vehicles\Scorpion\Arctic\Scorp_net_tundra_ca.paa",
+				"W_Units\data\Vehicles\Scorpion\Arctic\22nd_Scorp_Mine_Roller_Arctic_co.paa",
+				"W_Units\data\Vehicles\Scorpion\Arctic\22nd_Scorp_MG_Arctic_co.paa"
+			};
+		};
+	};
+};
+class 22nd_M808BM_Lancer_Scorpion_Driverless: OPTRE_M808BM_UNSC_Driverless
+{
+	scope=2;
+	author="Kidd";
+	displayName="[22nd] M808BM Lancer Scorpion Driverless";
+	faction="22nd_Faction";
+	editorCategory="22nd_Warhound_Company";
+	editorSubcategory="22nd_Warhound_Company_Tanks";
+	editorPreview="\OPTRE_Vehicles\Oryx\data\Preview\OPTRE_M494.jpg";
+	side=1;
+	hiddenSelectionsTextures[] =
+	{
+		"W_Units\data\Vehicles\Scorpion\Urban\22nd_Scorp_Base_Black_co.paa",
+		"W_Units\data\Vehicles\Scorpion\Lancer\22nd_Scorp_Tur_Lancer_Black_co.paa",
+		"W_Units\data\Vehicles\Scorpion\22nd_det_3_black_co.paa",
+		"W_Units\data\Vehicles\Scorpion\Lancer\22nd_scorp_decal_Lancer_co.paa",
+		"W_Units\data\Vehicles\Scorpion\Urban\Scorp_net_wood_ca.paa",
+		"W_Units\data\Vehicles\Scorpion\Urban\Scorp_net_wood_ca.paa",
+		"W_Units\data\Vehicles\Scorpion\Urban\22nd_Scorp_Mine_Roller_Black_co.paa",
+		"W_Units\data\Vehicles\Scorpion\Urban\22nd_Scorp_MG_Black_co.paa"
+	};
+	textureList[] =
+	{
+		"Lancer_Scorp_Urban_Driverless",
+		1,
+		"Lancer_Scorp_Woodland_Driverless",
+		0,
+		"Lancer_Scorp_Arid_Driverless",
+		0,
+		"Lancer_Scorp_Arctic_Driverless",
+		0,
+	};
+	class textureSources
+	{
+		class Lancer_Scorp_Urban_Driverless
+		{
+			displayName = "Lancer_Scorp_Urban_Driverless";
+			author = "Kidd";
+			textures[] =
+			{
+				"W_Units\data\Vehicles\Scorpion\Urban\22nd_Scorp_Base_Black_co.paa",
+				"W_Units\data\Vehicles\Scorpion\Lancer\22nd_Scorp_Tur_Lancer_Black_co.paa",
+				"W_Units\data\Vehicles\Scorpion\22nd_det_3_black_co.paa",
+				"W_Units\data\Vehicles\Scorpion\Lancer\22nd_scorp_decal_Lancer_co.paa",
+				"W_Units\data\Vehicles\Scorpion\Urban\Scorp_net_wood_ca.paa",
+				"W_Units\data\Vehicles\Scorpion\Urban\Scorp_net_wood_ca.paa",
+				"W_Units\data\Vehicles\Scorpion\Urban\22nd_Scorp_Mine_Roller_Black_co.paa",
+				"W_Units\data\Vehicles\Scorpion\Urban\22nd_Scorp_MG_Black_co.paa"
+			};
+		};
+		class Lancer_Scorp_Arid_Driverless
+		{
+			displayName = "Lancer_Scorp_Arid_Driverless";
+			author = "Kidd";
+			textures[] =
+			{
+				"W_Units\data\Vehicles\Scorpion\Arid\22nd_Scorp_Base_Desert_co.paa",
+				"W_Units\data\Vehicles\Scorpion\Lancer\22nd_Scorp_Tur_Lancer_Desert_co.paa",
+				"W_Units\data\Vehicles\Scorpion\22nd_det_3_black_co.paa",
+				"W_Units\data\Vehicles\Scorpion\Lancer\22nd_scorp_decal_Lancer_co.paa",
+				"W_Units\data\Vehicles\Scorpion\Arid\Scorp_net_desert_ca.paa",
+				"W_Units\data\Vehicles\Scorpion\Arid\Scorp_net_desert_ca.paa",
+				"W_Units\data\Vehicles\Scorpion\Arid\22nd_Scorp_Mine_Roller_Desert_co.paa",
+				"W_Units\data\Vehicles\Scorpion\Arid\22nd_Scorp_MG_Desert_co.paa"
+			};
+		};
+		class Lancer_Scorp_Woodland_Driverless
+		{
+			displayName = "Lancer_Scorp_Woodland_Driverless";
+			author = "Kidd";
+			textures[] =
+			{
+				"W_Units\data\Vehicles\Scorpion\Woodland\22nd_Scorp_Base_Woodland_co.paa",
+				"W_Units\data\Vehicles\Scorpion\Lancer\22nd_Scorp_Tur_Lancer_Woodland_co.paa",
+				"W_Units\data\Vehicles\Scorpion\22nd_det_3_black_co.paa",
+				"W_Units\data\Vehicles\Scorpion\Lancer\22nd_scorp_decal_Lancer_co.paa",
+				"W_Units\data\Vehicles\Scorpion\Woodland\Scorp_net_jungle_ca.paa",
+				"W_Units\data\Vehicles\Scorpion\Woodland\Scorp_net_jungle_ca.paa",
+				"W_Units\data\Vehicles\Scorpion\Woodland\22nd_Scorp_Mine_Roller_Woodland_co.paa",
+				"W_Units\data\Vehicles\Scorpion\Woodland\22nd_Scorp_MG_Woodland_co.paa"
+			};
+		};
+		class Lancer_Scorp_Arctic_Driverless
+		{
+			displayName = "Lancer_Scorp_Arctic_Driverless";
+			author = "Kidd";
+			textures[] =
+			{
+				"W_Units\data\Vehicles\Scorpion\Arctic\22nd_Scorp_Base_Arctic_co.paa",
+				"W_Units\data\Vehicles\Scorpion\Lancer\22nd_Scorp_Tur_Lancer_Arctic_co.paa",
+				"W_Units\data\Vehicles\Scorpion\22nd_det_3_black_co.paa",
+				"W_Units\data\Vehicles\Scorpion\Lancer\22nd_scorp_decal_Lancer_co.paa",
+				"W_Units\data\Vehicles\Scorpion\Arctic\Scorp_net_tundra_ca.paa",
+				"W_Units\data\Vehicles\Scorpion\Arctic\Scorp_net_tundra_ca.paa",
+				"W_Units\data\Vehicles\Scorpion\Arctic\22nd_Scorp_Mine_Roller_Arctic_co.paa",
+				"W_Units\data\Vehicles\Scorpion\Arctic\22nd_Scorp_MG_Arctic_co.paa"
 			};
 		};
 	};
@@ -213,13 +314,13 @@ class 22ndM808BFND_Scorpion: DMNS_M808B_F
 	canFloat = true;
 	hiddenSelectionsTextures[]=
 	{
-		"Warhound_Units\data\Vehicles\Scorpion\Lancer\M808B_Hull_co.paa",
-		"Warhound_Units\data\Vehicles\Scorpion\Lancer\M808B_Turret_co.paa",
-		"Warhound_Units\data\Vehicles\Scorpion\Lancer\M808B_Track_Housing_Front_co.paa",
-		"Warhound_Units\data\Vehicles\Scorpion\Lancer\M808B_Track_Housing_Rear_co.paa",
-		"Warhound_Units\data\Vehicles\Scorpion\Lancer\M808B_Commander_co.paa",
-		"Warhound_Units\data\Vehicles\Scorpion\Lancer\M808B_Details_EXT_co.paa",
-		"Warhound_Units\data\Vehicles\Scorpion\Lancer\M808B_Details_EXT_2_co.paa",
-		"Warhound_Units\data\Vehicles\Scorpion\Lancer\M808B_Track_Int_co.paa"
+		"W_Units\data\Vehicles\Scorpion\Lancer\M808B_Hull_co.paa",
+		"W_Units\data\Vehicles\Scorpion\Lancer\M808B_Turret_co.paa",
+		"W_Units\data\Vehicles\Scorpion\Lancer\M808B_Track_Housing_Front_co.paa",
+		"W_Units\data\Vehicles\Scorpion\Lancer\M808B_Track_Housing_Rear_co.paa",
+		"W_Units\data\Vehicles\Scorpion\Lancer\M808B_Commander_co.paa",
+		"W_Units\data\Vehicles\Scorpion\Lancer\M808B_Details_EXT_co.paa",
+		"W_Units\data\Vehicles\Scorpion\Lancer\M808B_Details_EXT_2_co.paa",
+		"W_Units\data\Vehicles\Scorpion\Lancer\M808B_Track_Int_co.paa"
 	};
 };
