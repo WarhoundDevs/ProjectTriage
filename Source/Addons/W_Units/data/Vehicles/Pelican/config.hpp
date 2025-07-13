c73a51ca09b1dc8b33bdb15c501b9e7b72cc2526
byorh2 <59620914+byorh2@users.noreply.github.com>
2025-07-13 14:09:40 -0400

    New Pelicans, plus editor category fix
    Added single seater pelican and SOCOM pelican, and fixed their editor category so they show up in the editor. SOCOM is WIP due to small texture issues.

--- Source/Addons/W_Units/data/Vehicles/Pelican/config.hpp
+++ Source/Addons/W_Units/data/Vehicles/Pelican/config.hpp
@@ -1,4 +1,6 @@
 class OPTRE_Pelican_armed;
+class OPTRE_Pelican_armed_single_seater;
+class OPTRE_Pelican_armed_SOCOM;
 class Warhound_Pelican_ARMY: OPTRE_Pelican_armed
 {
 	features="Randomization: No";
@@ -10,8 +12,8 @@ class Warhound_Pelican_ARMY: OPTRE_Pelican_armed
 	displayName="[22nd] D77H-TCI/AV Pelican";
 	faction="22nd_Faction";
 	crew="OPTRE_UNSC_Marine_Pilot";
-	editorCategory="22ndRDF_Warhound_Company";
-	editorSubcategory="22ndRDF_Warhound_Company_Air_Vehicles";
+	editorCategory="22nd_Warhound_Company";
+	editorSubcategory="22nd_Warhound_Company_Air_Vehicles";
 	hiddenSelections[]=
 	{
 		"camo1",
@@ -106,6 +108,202 @@ class Warhound_Pelican_ARMY: OPTRE_Pelican_armed
 		0,
 		"Manta",
 		0
+	};
+};
+class Warhound_Pelican_ARMY_Single_Seater: OPTRE_Pelican_armed_single_seater
+{
+	features="Randomization: No";
+	scope=2;
+	scopeCurator=2;
+	forceInGarage=1;
+	side=1;
+	author="The Canadian Nematoad";
+	displayName="[22nd] D77H-TCI/AV Pelican Single Seater";
+	faction="22nd_Faction";
+	crew="OPTRE_UNSC_Marine_Pilot";
+	editorCategory="22nd_Warhound_Company";
+	editorSubcategory="22nd_Warhound_Company_Air_Vehicles";
+	hiddenSelections[]=
+	{
+		"camo1",
+		"camo3",
+		"clan",
+		"clan_text",
+		"insignia"
+	};
+	hiddenSelectionsTextures[]=
+	{
+		"W_Units\data\Vehicles\Pelican\WH_Pelican_ARMY_BLACK_co.paa"
+	};
+	class TextureSources
+	{
+		class Arid_Single_Seater
+		{
+			displayName="Arid_Single_Seater";
+			author="Gatt";
+			textures[]=
+			{
+				"W_Units\data\Vehicles\Pelican\WH_Pelican_ARMY_ARID_co.paa",
+				""
+			};
+		};
+		class Night_Single_Seater
+		{
+			displayName="Night_Single_Seater";
+			author="Gatt";
+			textures[]=
+			{
+				"W_Units\data\Vehicles\Pelican\WH_Pelican_ARMY_NIGHT_co.paa",
+				""
+			};
+		};
+		class Black_Single_Seater
+		{
+			displayName="Black_Single_Seater";
+			author="Gatt";
+			textures[]=
+			{
+				"W_Units\data\Vehicles\Pelican\WH_Pelican_ARMY_BLACK_co.paa",
+				""
+			};
+		};
+		class Woodland_Single_Seater
+		{
+			displayName="Woodland_Single_Seater";
+			author="Gatt";
+			textures[]=
+			{
+				"W_Units\data\Vehicles\Pelican\WH_Pelican_ARMY_WOOD_co.paa",
+				""
+			};
+		};
+		class Arctic_Single_Seater
+		{
+			displayName="Arctic_Single_Seater";
+			author="Gatt";
+			textures[]=
+			{
+				"W_Units\data\Vehicles\Pelican\WH_Pelican_ARMY_ARCTIC_co.paa",
+				""
+			};
+		};
+		factions[]=
+		{
+			"BLU_F",
+			"BLU_T_F",
+			"22nd_Faction"
+		};
+	};
+	textureList[]=
+	{
+		"Arid_Single_Seater",
+		1,
+		"Woodland_Single_Seater",
+		0,
+		"Arctic_Single_Seater",
+		0,
+		"Night_Single_Seater",
+		0,
+		"Black_Single_Seater",
+		0,
+	};
+};
+class Warhound_Pelican_ARMY_SOCOM: OPTRE_Pelican_armed_SOCOM
+{
+	features="Randomization: No";
+	scope=2;
+	scopeCurator=2;
+	forceInGarage=1;
+	side=1;
+	author="The Canadian Nematoad";
+	displayName="[22nd] D77H-TCI/AV Pelican SOCOM (WIP)";
+	faction="22nd_Faction";
+	crew="OPTRE_UNSC_Marine_Pilot";
+	editorCategory="22nd_Warhound_Company";
+	editorSubcategory="22nd_Warhound_Company_Air_Vehicles";
+	hiddenSelections[]=
+	{
+		"camo1",
+		"camo3",
+		"clan",
+		"clan_text",
+		"insignia"
+	};
+	hiddenSelectionsTextures[]=
+	{
+		"W_Units\data\Vehicles\Pelican\WH_Pelican_ARMY_BLACK_co.paa"
+	};
+	class TextureSources
+	{
+		class Arid_SOCOM
+		{
+			displayName="Arid_SOCOM";
+			author="Gatt";
+			textures[]=
+			{
+				"W_Units\data\Vehicles\Pelican\WH_Pelican_ARMY_ARID_co.paa",
+				""
+			};
+		};
+		class Night_SOCOM
+		{
+			displayName="Night_SOCOM";
+			author="Gatt";
+			textures[]=
+			{
+				"W_Units\data\Vehicles\Pelican\WH_Pelican_ARMY_NIGHT_co.paa",
+				""
+			};
+		};
+		class Black_SOCOM
+		{
+			displayName="Black_SOCOM";
+			author="Gatt";
+			textures[]=
+			{
+				"W_Units\data\Vehicles\Pelican\WH_Pelican_ARMY_BLACK_co.paa",
+				""
+			};
+		};
+		class Woodland_SOCOM
+		{
+			displayName="Woodland_SOCOM";
+			author="Gatt";
+			textures[]=
+			{
+				"W_Units\data\Vehicles\Pelican\WH_Pelican_ARMY_WOOD_co.paa",
+				""
+			};
+		};
+		class Arctic_SOCOM
+		{
+			displayName="Arctic_SOCOM";
+			author="Gatt";
+			textures[]=
+			{
+				"W_Units\data\Vehicles\Pelican\WH_Pelican_ARMY_ARCTIC_co.paa",
+				""
+			};
+		};
+		factions[]=
+		{
+			"BLU_F",
+			"BLU_T_F",
+			"22nd_Faction"
+		};
+	};
+	textureList[]=
+	{
+		"Arid_SOCOM",
+		1,
+		"Woodland_SOCOM",
+		0,
+		"Arctic_SOCOM",
+		0,
+		"Night_SOCOM",
+		0,
+		"Black_SOCOM",
+		0,
 	};
 };
 
