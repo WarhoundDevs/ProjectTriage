c763bb712082935f9b517bdadcafac192c4fa283
Louis Partington <louispartington1@hotmail.co.uk>
2026-04-08 00:02:45 +0100

    Revert "Fix winter open helms to use correct textures"

--- Source/Addons/W_Uniform/W_Helmets/config.cpp
+++ Source/Addons/W_Uniform/W_Helmets/config.cpp
@@ -576,9 +576,9 @@ class CfgWeapons {
             };
               hiddenSelectionsTextures[] =
               {
-                  "\W_Uniform\W_Armour\Infantry\22nd_Helmet_Snow.paa",
-                  "\W_Uniform\W_Armour\Infantry\22nd_Helmet_Snow.paa",
-                  "\W_Uniform\W_Armour\Infantry\22nd_Helmet_Snow.paa",
+            "MA_Armor\data\Helmets\CH252\data\MA_HelmetAccessories_A_CO.paa",
+            "MA_Armor\data\Helmets\CH252\data\MA_HelmetAccessories_A_CO.paa",
+            "MA_Armor\data\Helmets\CH252\data\MA_HelmetAccessories_A_CO.paa",
                   "MA_Armor\data\Helmets\CH252\data\MA_HelmetAccessories_A_CO.paa",
                   "MA_Armor\data\Helmets\CH252\data\MA_HelmetAccessories_A_CO.paa",
                   "MA_Armor\data\Helmets\CH252\data\MA_HelmetAccessories_A_CO.paa",
@@ -746,9 +746,9 @@ class CfgWeapons {
             };
               hiddenSelectionsTextures[] =
               {
-                  "\W_Uniform\W_Armour\Infantry\22nd_Helmet_Snow.paa",
-                  "\W_Uniform\W_Armour\Infantry\22nd_Helmet_Snow.paa",
-                  "\W_Uniform\W_Armour\Infantry\22nd_Helmet_Snow.paa",
+            "MA_Armor\data\Helmets\CH252\data\MA_HelmetAccessories_A_CO.paa",
+            "MA_Armor\data\Helmets\CH252\data\MA_HelmetAccessories_A_CO.paa",
+            "MA_Armor\data\Helmets\CH252\data\MA_HelmetAccessories_A_CO.paa",
                   "MA_Armor\data\Helmets\CH252\data\MA_HelmetAccessories_A_CO.paa",
                   "MA_Armor\data\Helmets\CH252\data\MA_HelmetAccessories_A_CO.paa",
                   "MA_Armor\data\Helmets\CH252\data\MA_HelmetAccessories_A_CO.paa",
