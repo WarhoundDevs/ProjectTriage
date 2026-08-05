fb5d8e80fa3d6dad165e4b13eede485020593fea
Literally Me <spacegod12345@gmail.com>
2026-08-05 09:44:08 -0400

    Bags update
    Believe the invis backpack is still broken on the radio side (unable to test)
    
    thank you MJ

--- addons/uniforms/bags/config.cpp
+++ addons/uniforms/bags/config.cpp
@@ -1,6 +1,6 @@
 class cfgPatches
 {
-	class OPTRE_22nd_uniforms_bags
+	class 22nd_uniforms_bags
 	{
 		author="The Warhound Mod Team";
 		units[]=
@@ -19,69 +19,9 @@ class cfgPatches
 		};
 	};
 };
+#include "infantryXtdGear.hpp"
 
-//Added AAE functionality - 0905, Hertzel
 
-class XtdGearModels
-{
-    class CfgVehicles
-    {
-        class WBag_AAE
-        {
-            label = "[22nd] Rucksacks";
-            author = "Warhound Mod Team";
-            options[] =
-            {
-                "Camoflauge",
-                "Variant"
-            };
-            class Camoflauge
-            {
-                label = "Camoflauge";
-                values[] =
-                {
-                    "MCAM",
-                    "Snow",
-                    "Woodland",
-                };
-                class MCAM
-                {
-                    label = "MCAM";
-                };
-                class Snow
-                {
-                    label = "Snow";
-                };
-                class Woodland
-                {
-                    label = "Woodland";
-                };
-            };
-            class Variant
-            {
-                label = "Variant";
-                values[] =
-                {
-                    "Regular",
-                    "Medical",
-					"Heavy",
-                };
-                class Regular
-                {
-                    label = "Regular";
-                };
-                class Medical
-                {
-                    label = "Medical";
-                };
-                class Heavy
-                {
-                    label = "Heavy";
-                };
-            };
-        };
-	};
-};
 class CfgVehicles
 {
 	class OPTRE_UNSC_Rucksack;
@@ -94,7 +34,7 @@ class CfgVehicles
 		scope=2;
 		scopeCurator=2;
 		displayName="AI Radio Integration";
-		model="W_Uniform\W_Bags\data\WH_Invis_Tas.p3d";
+		model="z\22ndRDF\addons\uniforms\bags\data\WH_Invis_Tas.p3d";
 		maximumLoad=250;
 		tf_encryptionCode="tf_west_radio_code";
 		tf_dialog="rt1523g_radio_dialog";
@@ -109,7 +49,7 @@ class CfgVehicles
 		scope=2;
 		scopeCurator=2;
 		displayName="[22nd] Invisible Backpack";
-		model="W_Uniform\W_Bags\data\WH_Invis_Tas.p3d";
+		model="z\22ndRDF\addons\uniforms\bags\data\WH_Invis_Tas.p3d";
 		maximumLoad=250;
 		hiddenSelections[]=
 		{
@@ -141,13 +81,13 @@ class CfgVehicles
 		};
         class XtdGearInfo
         {
-            model = "WBag_AAE";
+            model = "22nd_Backpacks";
             Camoflauge = "MCAM";
             Variant = "Regular";
         };
 		hiddenSelectionsTextures[]=
 		{
-			"W_Uniform\W_Bags\data\Soft_Backpack_WH_co.paa"
+			"z\22ndRDF\addons\uniforms\bags\data\soft_backpack_WH_co.paa"
 		};
 	};
 	class WH_Rucksack_Soft_Medical: OPTRE_UNSC_Rucksack_Medic
@@ -165,13 +105,13 @@ class CfgVehicles
 		};
 		class XtdGearInfo
         {
-            model = "WBag_AAE";
+            model = "22nd_Backpacks";
             Camoflauge = "MCAM";
             Variant = "Medical";
         };
 		hiddenSelectionsTextures[]=
 		{
-			"W_Uniform\W_Bags\data\Soft_Backpack_WH_Medical_co.paa"
+			"z\22ndRDF\addons\uniforms\bags\data\Soft_Backpack_WH_Medical_co.paa"
 		};
 	};
 	class WH_Rucksack_Soft_Heavy: OPTRE_UNSC_Rucksack_Heavy
@@ -189,13 +129,13 @@ class CfgVehicles
 		};
 		class XtdGearInfo
         {
-            model = "WBag_AAE";
+            model = "22nd_Backpacks";
             Camoflauge = "MCAM";
             Variant = "Heavy";
         };
 		hiddenSelectionsTextures[]=
 		{
-			"W_Uniform\W_Bags\data\Soft_Backpack_WH_co.paa"
+			"z\22ndRDF\addons\uniforms\bags\data\Soft_Backpack_WH_co.paa"
 		};
 	};
 	class WH_Rucksack_Soft_Snow: OPTRE_UNSC_Rucksack
@@ -215,13 +155,13 @@ class CfgVehicles
 		};
 		class XtdGearInfo
         {
-            model = "WBag_AAE";
+            model = "22nd_Backpacks";
             Camoflauge = "Snow";
             Variant = "Regular";
         };
 		hiddenSelectionsTextures[]=
 		{
-			"W_Uniform\W_Bags\data\Soft_Backpack_WH_Snow_co.paa"
+			"z\22ndRDF\addons\uniforms\bags\data\Soft_Backpack_WH_Snow_co.paa"
 		};
 	};
 	class WH_Rucksack_Soft_Medical_Snow: OPTRE_UNSC_Rucksack_Medic
@@ -239,13 +179,13 @@ class CfgVehicles
 		};
 		class XtdGearInfo
         {
-            model = "WBag_AAE";
+            model = "22nd_Backpacks";
             Camoflauge = "Snow";
             Variant = "Medical";
         };
 		hiddenSelectionsTextures[]=
 		{
-			"W_Uniform\W_Bags\data\Soft_Backpack_WH_Medical_Snow_co.paa"
+			"z\22ndRDF\addons\uniforms\bags\data\Soft_Backpack_WH_Medical_Snow_co.paa"
 		};
 	};
 	class WH_Rucksack_Soft_Heavy_Snow: OPTRE_UNSC_Rucksack_Heavy
@@ -263,13 +203,13 @@ class CfgVehicles
 		};
 		class XtdGearInfo
         {
-            model = "WBag_AAE";
+            model = "22nd_Backpacks";
             Camoflauge = "Snow";
             Variant = "Heavy";
         };
 		hiddenSelectionsTextures[]=
 		{
-			"W_Uniform\W_Bags\data\Soft_Backpack_WH_Snow_co.paa"
+			"z\22ndRDF\addons\uniforms\bags\data\Soft_Backpack_WH_Snow_co.paa"
 		};
 	};
 	class WH_Rucksack_Soft_WDL: OPTRE_UNSC_Rucksack
@@ -289,13 +229,13 @@ class CfgVehicles
 		};
 		class XtdGearInfo
         {
-            model = "WBag_AAE";
+            model = "22nd_Backpacks";
             Camoflauge = "Woodland";
             Variant = "Regular";
         };
 		hiddenSelectionsTextures[]=
 		{
-			"W_Uniform\W_Bags\data\Soft_Backpack_WH_WDL_co.paa"
+			"z\22ndRDF\addons\uniforms\bags\data\Soft_Backpack_WH_WDL_co.paa"
 		};
 	};
 	class WH_Rucksack_Soft_Medical_WDL: OPTRE_UNSC_Rucksack_Medic
@@ -313,13 +253,13 @@ class CfgVehicles
 		};
 		class XtdGearInfo
         {
-            model = "WBag_AAE";
+            model = "22nd_Backpacks";
             Camoflauge = "Woodland";
             Variant = "Medical";
         };
 		hiddenSelectionsTextures[]=
 		{
-			"W_Uniform\W_Bags\data\Soft_Backpack_WH_Medical_WDL_co.paa"
+			"z\22ndRDF\addons\uniforms\bags\data\Soft_Backpack_WH_Medical_WDL_co.paa"
 		};
 	};
 	class WH_Rucksack_Soft_Heavy_WDL: OPTRE_UNSC_Rucksack_Heavy
@@ -337,13 +277,13 @@ class CfgVehicles
 		};
 		class XtdGearInfo
         {
-            model = "WBag_AAE";
+            model = "22nd_Backpacks";
             Camoflauge = "Woodland";
             Variant = "Heavy";
         };
 		hiddenSelectionsTextures[]=
 		{
-			"W_Uniform\W_Bags\data\Soft_Backpack_WH_WDL_co.paa"
+			"z\22ndRDF\addons\uniforms\bags\data\Soft_Backpack_WH_WDL_co.paa"
 		};
 	};
 };
---
+++ addons/uniforms/bags/infantryXtdGear.hpp
@@ -0,0 +1,62 @@
+class XtdGearModels
+{
+    class CfgVehicles
+    {
+        class 22nd_Backpacks
+        {
+            label = "[22nd] Rucksacks";
+            author = "Warhound Mod Team";
+            options[] =
+            {
+                "Camoflauge",
+                "Variant"
+            };
+             class Camoflauge
+            {
+                label = "Camoflauge";
+                values[] =
+                {
+                    "MCAM",
+                    "Snow",
+                    "Woodland",
+                };
+                class MCAM
+                {
+                    label = "MCAM";
+                };
+                class Snow
+                {
+                    label = "Snow";
+                };
+                class Woodland
+                {
+                    label = "Woodland";
+                };
+            };
+            class Variant
+            {
+                label = "Variant";
+                values[] =
+                {
+                    "Regular",
+                    "Medical",
+					"Heavy",
+                };
+                class Regular
+                {
+                    label = "Regular";
+                };
+                class Medical
+                {
+                    label = "Medical";
+                };
+                class Heavy
+                {
+                    label = "Heavy";
+                };
+            };
+        };
+    };
+};
+       
+       
